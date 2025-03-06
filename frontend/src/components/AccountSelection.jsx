3a0a451979c1f410ba0015307797f90e70ba53b8
Utkarsh Pandey <140243045+UtkarshP07@users.noreply.github.com>
2025-03-06 23:43:54 +0530

    Update AccountSelection.jsx
    Just a normal change

--- frontend/src/components/AccountSelection.jsx
+++ frontend/src/components/AccountSelection.jsx
@@ -12,7 +12,7 @@ const AccountSelection = ({ onAccountSelect }) => {
     <><div className="mt-2"><h1 className="text-gray-500 dark:text-gray-300 font-semibold">Select Your Category :</h1></div>
     <div className="flex justify-center mt-2">
       <div className="mt-1 md:flex md:items-center md:-mx-2">
-        {["Farmer", "Manufacturer", "Transporter"].map((name) => (
+        {["Farmer", "Manufacture", "Transporter"].map((name) => (
           <button
             key={name}
             className={`font-semibold flex justify-center w-full px-6 py-3 mt-2 md:mt-0 md:w-auto md:mx-2 rounded-md focus:outline-none transition-all ${
